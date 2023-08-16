fbeba4009055a6774149aa812ac011d8a2c730cb
JulioSCaseiro <caseiroinc@gmail.com>
2023-08-16 15:39:14 +0100

    Init project

---
+++ WaterCompanyWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WaterCompanyWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
